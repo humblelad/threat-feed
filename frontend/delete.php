b971abfa3b62f90ebbc5c92278629cf2a95322f4
humblelad <30574278+humblelad@users.noreply.github.com>
2021-11-09 11:08:16 +0530

    prep stmt

--- frontend/delete.php
+++ frontend/delete.php
@@ -4,8 +4,11 @@ include "connection.php";
 if(isset($_POST['id'])){
    $id=$_POST['id'];
 
-   $sql=pg_query($dbconn,"INSERT INTO delete(deleteid) VALUES('$id')");   
-   $sql=pg_query($dbconn,"DELETE FROM review WHERE videoid='$id'");
+      //commented below due as we already send to delete table before sending to review table. 	
+   //$sql=pg_query($dbconn,"INSERT INTO delete(deleteid) VALUES('$id')");
+     //$sql=pg_query($dbconn,"DELETE FROM review WHERE videoid='$id'");
+   
+$sql=pg_query($dbconn,"PREPARE revdel(text) as DELETE FROM review i WHERE i.videoid=$1; EXECUTE revdel('$id');");
 echo 1;
 pg_close($dbconn);
 exit;
