83ceb35fe14cb368f8bfd8a7e7b8a90e61c5a4c1
humblelad <30574278+humblelad@users.noreply.github.com>
2021-11-09 11:06:52 +0530

    prepared stmt

--- frontend/ajaxfile.php
+++ frontend/ajaxfile.php
@@ -5,9 +5,12 @@ include "connection.php";
 if($_POST['r']==1){
 	$id=$_POST['id'];
 	
-	$sql=pg_query($dbconn,"INSERT INTO review VALUES('$id')");
-   $sql=pg_query($dbconn,"INSERT INTO DELETE VALUES('$id')");
-	$sql=pg_query($dbconn,"DELETE FROM initial WHERE videoid='$id'");
+	// $sql=pg_query($dbconn,"INSERT INTO review VALUES('$id')");
+ //   $sql=pg_query($dbconn,"INSERT INTO delete VALUES('$id')");
+	// $sql=pg_query($dbconn,"DELETE FROM initial WHERE videoid='$id'");
+$sql=pg_query($dbconn,"PREPARE rev(text) as INSERT INTO review VALUES($1); EXECUTE rev('$id');");
+$sql=pg_query($dbconn,"PREPARE transfer(text) as INSERT INTO delete VALUES($1); EXECUTE transfer('$id');");
+$sql=pg_query($dbconn,"PREPARE bahar(text) as DELETE FROM initial i WHERE i.videoid=$1; EXECUTE bahar('$id');");
 	echo 1;
 	exit;
 }
@@ -15,8 +18,10 @@ if($_POST['r']==1){
 if(isset($_POST['id'])){
    $id=$_POST['id'];
 
-   $sql=pg_query($dbconn,"INSERT INTO DELETE VALUES('$id')");
-   $sql=pg_query($dbconn,"DELETE FROM initial WHERE videoid='$id'");
+ $sql=pg_query($dbconn,"PREPARE andar(text) as INSERT INTO DELETE VALUES($1); EXECUTE andar('$id');");
+   $sql=pg_query($dbconn,"PREPARE bahar(text) as DELETE FROM initial i WHERE i.videoid=$1; EXECUTE bahar('$id');");
+   //$sql=pg_query($dbconn,"INSERT INTO DELETE VALUES('$id')");
+   //$sql=pg_query($dbconn,"DELETE FROM initial WHERE videoid='$id'");
    
    echo 1;
    pg_close($dbconn);
